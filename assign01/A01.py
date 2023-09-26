e5afaa0b9a5ad5e1018689e63ee541ee71aa0e28
Michael Giardina <giardim@sunypoly.edu>
2023-09-26 09:18:17 -0400

    worked on assignment 1 3/5 test cases

--- assign01/A01.py
+++ assign01/A01.py
@@ -1,12 +1,14 @@
 import numpy as np
 import cv2
 import gradio as gr
+import math as m
 
 def create_unnormalized_hist(image):
     hist = np.zeros(256, dtype="float32")
-    for i in range(len(image)):
-        pixel_value = image[i]
-        hist[pixel_value] += 1
+    for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
+            pixel_value = image[i][j]
+            hist[pixel_value] += 1
     return hist
    
 def normalize_hist(hist):
@@ -29,21 +31,25 @@ def get_hist_equalize_transform(image, do_stretching, do_cl=False, cl_thresh=0):
     if do_stretching:
         first_val = cdf[0]
         last_val = cdf[255]
-        for i in range(len(cdf)):
-            cdf[i]  -= first_val 
+        for i in range(cdf.shape[0]):
+            cdf[i]  -= first_val
             cdf[i] /= last_val
-    for i in range(len(cdf)):
-        cdf[i] *= 255
-    int_transform = cv2.convertScaleAbs(int_transform)[:,0]
+    for i in range(cdf.shape[0]):
+        cdf[i] *= 255.0
+        
+    
+    int_transform = cv2.convertScaleAbs(cdf)[:,0]
     return int_transform
     
 def do_histogram_equalize(image, do_stretching):
     output = np.copy(image)
     int_transform = get_hist_equalize_transform(image, do_stretching)
-    for i in range(len(image)):
-        val = image[i]
-        new_val = val * int_transform
-        output[i] = new_val
+    for i in range (image.shape[0]):
+        for j in range(image.shape[1]):
+            pixel_value = image[i][j]
+            new_value = pixel_value * int_transform[pixel_value]
+            output[i][j] = new_value
+        
     return output
     
 def intensity_callback(input_img, do_stretching):

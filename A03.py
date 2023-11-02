9bb383488f74e6a146fbf97f91164f2499b65911
Michael Giardina <giardim@sunypoly.edu>
2023-11-02 14:51:28 -0400

    95% training 93% test

--- A03.py
+++ A03.py
@@ -4,7 +4,7 @@ import numpy as np
 
 def find_WBC(image):
     #Step 1 get superpixel groups
-    segments = skimage.segmentation.slic(image, n_segments=50, sigma=5, start_label=0)
+    segments = skimage.segmentation.slic(image, n_segments=40, sigma=8, start_label=0)
     cnt = len(np.unique(segments))
     
     #Step 2 compute mean per superpixel

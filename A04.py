2b50a8b117e48eaa9dd9e5d2a19cc76664e9ceba
Michael Giardina <giardim@sunypoly.edu>
2023-11-14 16:00:26 -0500

    1/4 tests passed

--- A04.py
+++ A04.py
@@ -1,4 +1,5 @@
 import numpy as np
+import cv2 
 
 def getOneLBPLabel(subimage, label_type):
     center = subimage[1][1]
@@ -35,5 +36,14 @@ def getOneLBPLabel(subimage, label_type):
     return label
 
 def getLBPImage(image, label_type):
-    return image
+    output = np.copy(image)
+    subImage = np.zeros(shape=(4,4))
+    for i in range (image.shape[0]):
+        for j in range (image.shape[1]):
+            subImage = image[i:i+3, j:j+3]
+            if (subImage.shape[0] < 3 or subImage.shape[1] < 3):
+                continue
+            output[i][j] = getOneLBPLabel(subImage, label_type="UNIFORM")
+    return output
+
 

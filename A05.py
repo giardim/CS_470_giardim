5e7d08a5e29693ed48f9938f62a78e1ccd0cfd4e
Michael Giardina <giardim@sunypoly.edu>
2023-12-05 14:30:05 -0500

    fixed models, both models have 80%+ accuracy

--- A05.py
+++ A05.py
@@ -42,130 +42,61 @@ class CNN1(nn.Module):
         self.CNN1 = nn.Sequential(
             nn.Conv2d(3, 32, 3, padding="same"),
             nn.Dropout(.05),
-            nn.Sigmoid(),
-            nn.Conv2d(32, 32, 3, padding="same"),
-            nn.Sigmoid(),
-            nn.MaxPool2d(2),
-            
+            nn.ReLU(),
             nn.Conv2d(32, 64, 3, padding="same"),
-            nn.Dropout(.05),
-            nn.Sigmoid(),
+            nn.ReLU(),
+            nn.MaxPool2d(1),
+            
             nn.Conv2d(64, 64, 3, padding="same"),
-            nn.Sigmoid(),
-            nn.MaxPool2d(2),
+            nn.Dropout(.05),
+            nn.ReLU(),
+            nn.Conv2d(64, 128, 3, padding="same"),
+            nn.ReLU(),
+            nn.MaxPool2d(1),
             
             nn.Flatten(),
             
-            nn.Linear(4096, 32),
-            nn.Dropout(.05),
-            nn.Sigmoid(),
-            nn.Linear(32, class_cnt)
+            nn.Linear(131072, 32, bias=False),
+            nn.ReLU(),
+            nn.Linear(32, class_cnt, bias=False)
         )
         
     def forward(self, x):
         logits = self.CNN1(x)
         return logits
     
-class CNN2(nn.Module):
-    def __init__(self, class_cnt):
-        super().__init__()      
-        self.CNN2 = nn.Sequential(
-            nn.Linear(32, 3, True),
-            nn.ReLU(),
-            nn.Conv2d(32, 64, 3, padding="same"),
-            nn.ReLU(),
-            nn.Linear(64, 64, True),
-            nn.ReLU(),
-            
-            nn.Linear(64, 64, True),
-            nn.ReLU(),
-            nn.Conv2d(64, 64, 3, padding="same"),
-            nn.ReLU(),
-            nn.Linear(64, 64, True),
-            
-            nn.ReLU(),
-            
-            nn.Linear(64, 64, True),
-            nn.ReLU(),
-            nn.Conv2d(32, 64, 3, padding="same"),
-            nn.ReLU(),
-            nn.Linear(64, 64, True),
-            
-            nn.ReLU(),
-            
-            nn.Flatten(),
-            nn.Linear(6144, class_cnt)
-        )
         
-    def forward(self, x):
-        logits = self.CNN2(x)
-        return logits
-    
-class CNN3(nn.Module):
-    def __init__(self, class_cnt):
-        super().__init__()      
-        self.CNN3 = nn.Sequential(
-            nn.Conv2d(3, 64, 1, stride=1),
-            nn.ReLU(),
-            nn.Conv2d(64, 64, 1, stride=1),
-            nn.ReLU(),
-            
-            nn.Conv2d(64, 64, 1, stride=1),
-            nn.ReLU(),
-            nn.Conv2d(64, 64, 1, stride=1),
-            nn.ReLU(),
-            
-            nn.Conv2d(64, 64, 1, stride=2),
-            nn.ReLU(),
-            nn.Conv2d(64, 64, 1, stride=2),
-            nn.ReLU(),
-            
-            nn.MaxPool2d(5),
-            nn.Flatten(),
-            
-            nn.Linear(64, 32),
-            nn.ReLU(),
-            nn.Linear(32, class_cnt)
-        )
-        
-    def forward(self, x):
-        logits = self.CNN3(x)
-        return logits
-
 #Returns all model names, we can pick from this list later
 def get_approach_names():
-    approach_names = ["CNN0", "CNN1", "CNN2", "CNN3"]
+    approach_names = ["CNN0", "CNN1"]
     return approach_names
 
 #Returns the description of all layers, used in our evaluation
 def get_approach_description(approach_name):
     desc = {
-        "CNN0" : "14 Layer CNN with Leaky ReLU as the activation function. Uses convolution layers to transform the data.",
-        "CNN1" : "17 Layer CNN with Sigmoid as the activation function. Uses convolution layers to transform the data. Also has a dropout rate of 5%. Data is randomly flipped horizontally.",
-        "CNN2" : "20 Layer CNN with ReLu as the activation function. Uses linear layers to transform the data. Data is randomly flipped vertically.",
-        "CNN3" : "17 Layer CNN with ReLu as the activation function. Uses convolution to transform the data. One of the convolution layers has a stride length of 2."
+        "CNN0" : f"A model with convolutional layers that go up to size 64 and a padding of 'same'. There is also a pooling layer of stride 2. This model uses the leaky RElU activation function. While training, this model also randomly flips images horizontally.",
+        "CNN1" : "A model with convolutional layers that go up to size 128 and a padding of 'same'. There is also a pooling layer of stride 1. This model uses the RElU activation function and has 2 dropout layers with 15% probability. While training, this model also randomly flips images vertically.",
     }
     
     return desc[approach_name]
 
 #Data augmenation
 def get_data_transform(approach_name, training):
-    if (not training or approach_name == "CNN0" or approach_name == "CNN3"):
+    if (not training):
         data_transform = v2.Compose([
-        v2.ToImage(),
-        v2.ToDtype(torch.float32, scale=True)
-        ])
+        v2.ToImageTensor(), 
+        v2.ConvertImageDtype()])
     #Data is randomly flipped horizontally
-    elif (approach_name == "CNN1"):
+    elif (approach_name == "CNN0"):
         data_transform = v2.Compose([
-        v2.ToImage(),
-        v2.ToDtype(torch.float32, scale=True),
+        v2.ToImageTensor(), 
+        v2.ConvertImageDtype(),
         v2.RandomHorizontalFlip()])   
     #Data is randomly flipped vertically
-    elif (approach_name == "CNN2"):
+    elif (approach_name == "CNN1"):
         data_transform = v2.Compose([
-        v2.ToImage(),
-        v2.ToDtype(torch.float32, scale=True),
+        v2.ToImageTensor(), 
+        v2.ConvertImageDtype(),
         v2.RandomVerticalFlip()]) 
         
     
@@ -176,10 +107,6 @@ def get_batch_size(approach_name):
     if (approach_name == "CNN0"):
         batch_size = 64
     elif (approach_name == "CNN1"):
-        batch_size = 128
-    elif (approach_name == "CNN2"):
-        batch_size = 256
-    else:
         batch_size = 32
     
     return batch_size
@@ -190,10 +117,7 @@ def create_model (approach_name, class_cnt):
         model = CNN0(class_cnt)
     elif (approach_name == "CNN1"):
         model = CNN1(class_cnt)
-    elif (approach_name == "CNN2"):
-        model = CNN2(class_cnt)
-    elif (approach_name == "CNN3"):
-        model = CNN3(class_cnt)
+        
     return model
 
 #Trains the model
